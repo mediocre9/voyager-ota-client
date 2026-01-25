504c6a1be6d8e078a9b93463b44f6cf16be0d836
Mediocre <76650975+mediocre9@users.noreply.github.com>
2026-01-25 19:04:28 +0500

    V2 Update Merge (#6)
    * feat: Add `setCredentials` method
    
    * refactor: rename `connectToInternet` to `connectToWifi` and using `setCredentials`
    
    * fix: correct order of parameters in `setCredentials` method
    
    * Breaking change v2 lib features and core backend breaking change api integration (#3)
    
    * feat: add and integrate breaking API contract changes with voyagerOTA platform
    
    - Add `__ENABLE_ADVANCED_MODE__` and `__ENABLE_DEVELOPMENT_MODE__` mode compile time flags
    - refactor and clean codebase
    - Add `BaseModel` contract abstract class
    - Embed marker strings for build type binary identification
    
    * add `.clang-format` file
    
    * update examples for usage as per new lib features and integration
    
    * add structured `staging` api endpoints and headers
    
    * chores: increment lib version to 2.0.0 and update dependencies
    
    * Feat/v2 (#5)
    
    * feat: add and integrate breaking API contract changes with voyagerOTA platform
    
    - Add `__ENABLE_ADVANCED_MODE__` and `__ENABLE_DEVELOPMENT_MODE__` mode compile time flags
    - refactor and clean codebase
    - Add `BaseModel` contract abstract class
    - Embed marker strings for build type binary identification
    
    * add `.clang-format` file
    
    * update examples for usage as per new lib features and integration
    
    * add structured `staging` api endpoints and headers
    
    * chores: increment lib version to 2.0.0 and update dependencies
    
    * fix the usage of query param in `__ENABLE_DEVELOPMENT_MODE__` macro that incorrectly results in `production` release in enabled development mode
    
    - Add missing return statement in `VoyagerJSONParser`  that causes hard firmware crash when new update is not available
    
    * resolved #4 update breaking change api url

---
+++ src/ApiConstants.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+namespace __VoyagerApi__ {
+    String BASE_URL = "https://staging.api.voyagerota.com";
+
+    namespace Endpoints {
+        String LATEST_RELEASE = "/internal/api/v1/releases/latest";
+        // namespace QueryParams
+    }  // namespace Endpoints
+    namespace QueryParams {
+        String PRODUCTION_CHANNEL = "?channel=production";
+        String STAGING_CHANNEL = "?channel=staging";
+    }  // namespace QueryParams
+    namespace Headers {
+        namespace Keys {
+            constexpr const char* X_API_KEY = "x-api-key";
+            constexpr const char* X_PROJECT_ID = "x-project-id";
+        }  // namespace Keys
+    }  // namespace Headers
+}  // namespace __VoyagerApi__
